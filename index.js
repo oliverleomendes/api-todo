0bdb93c4a0bf66999a4eaffab0ced32b690c876c
LEONARDO MENDES DE OLIVEIRA <leonardo.moliveira25@senacsp.edu.br>
2025-04-11 18:47:51 -0300

    Atualização do projeto e adição de novo pacote

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 let tarefas = [
   { id: 1, titulo: "Estudar Node.js", concluida: false },
